59885fc64eebb09fb36eee9ab6524efe23d8ab0e
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-09 11:00:59 -0300

    Colorama

--- jogo.py
+++ jogo.py
@@ -2,7 +2,9 @@
 import random
 from math import *
 import funcoes
+import colorama
+from colorama import Fore, Back, Style, init
 
 tentativas=20
 
-print('\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
+print(Fore.CYAN + '\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
