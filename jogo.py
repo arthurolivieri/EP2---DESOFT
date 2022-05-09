73e9a00df1e801be8d06daed0e786c1650dd5de4
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-09 11:14:21 -0300

    desistimos do colorama

--- jogo.py
+++ jogo.py
@@ -2,9 +2,7 @@
 import random
 from math import *
 import funcoes
-import colorama
-from colorama import Fore, Back, Style, init
 
 tentativas=20
 
-print(Fore.CYAN + '\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
+print('\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
