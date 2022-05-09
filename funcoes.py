d115537c03d0d7c2808ce2351d491c9eca87f424
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-09 10:13:17 -0300

    funcoes

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 import random
+from math import *
 
 def normaliza(dic):
     dicnorm = {}
@@ -11,3 +12,63 @@ def normaliza(dic):
 def sorteia_pais(dic):
     pais = random.choice(list(dic))
     return pais
+
+def adiciona_em_ordem(nome,dist,lista):
+    saida=lista
+    if not lista:
+        saida.append([nome,dist])
+    for i in range (len(lista)):
+        if dist<lista[i][1]:
+            saida.insert(i,[nome,dist])
+            break
+    return saida
+
+
+def esta_na_lista(nome,lista):
+    flag=False
+    for i in range (len(lista)):
+        if nome == lista[i][0]:
+            flag=True
+    return flag
+
+def sorteia_letra(palavra,restrito):
+    l=list(palavra.lower())
+    if ' ' in l:
+        try:
+            while True:
+                l.remove(' ')
+        except ValueError:
+            pass
+    if '-' in l:
+        try:
+            while True:
+                l.remove('-')
+        except ValueError:
+            pass
+    if ',' in l:
+        l.remove(',')
+    print(restrito)
+    for i in range (len(restrito)):
+        if restrito[i] in l:
+            try:
+                while True:
+                    l.remove(restrito[i])
+            except ValueError:
+                pass
+    if not l:
+        return ""
+    res = []
+    for i in l:
+        if i not in res:
+            res.append(i)
+    else:
+        caracter=random.choice(res)
+        return caracter
+
+def haversine(r,p1,l1,p2,l2):
+    p=(p2-p1)/2
+    l=(l2-l1)/2
+    c1=sin(p)**2+(cos(p1)*cos(p2)*sin(l)**2)
+    c2=c1**(1/2)
+    d=2*r*asin(c2)
+    return d
