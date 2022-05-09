f26439e9cf0e514614ac893b71bae76d07a7dd36
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-09 11:27:31 -0300

    cores

--- jogo.py
+++ jogo.py
@@ -5,4 +5,4 @@ import funcoes
 
 tentativas=20
 
-print('\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
+print('\033[36m + \n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição'+ '\036[0m')
