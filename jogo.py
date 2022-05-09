870ffe7152e2c4454786fd90bc2088439a9d34eb
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-09 10:33:09 -0300

    Tela de início

--- jogo.py
+++ jogo.py
@@ -1,4 +1,7 @@
-import colorama
+
 import random
 from math import *
 
+tentativas=20
+
+print('\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
