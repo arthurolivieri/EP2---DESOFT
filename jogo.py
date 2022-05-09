ef4affc30f610dedd81a7bca153fd9f1616d2a03
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-09 11:42:47 -0300

    Input da tentativa

--- jogo.py
+++ jogo.py
@@ -2,7 +2,14 @@
 import random
 from math import *
 import funcoes
+import dados
 
 tentativas=20
 
 print('\033[36m + \n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição'+ '\036[0m')
+
+paises=funcoes.normaliza(dados.DADOS)
+
+
+resposta=funcoes.sorteia_pais(paises)
+tentativa=input('Você tem', tentativas,'\nQual o seu palpite?')
