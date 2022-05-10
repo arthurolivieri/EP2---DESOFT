40344ea01fca0738fd6964020ce546976782422d
Yuri Tabacof <yuritabacof@gmail.com>
2022-05-10 09:54:08 -0300

    sorteia pais + primeiro palpite

--- jogo.py
+++ jogo.py
@@ -4,12 +4,28 @@ from math import *
 import funcoes
 import dados
 
-tentativas=20
+vidas=20
 
-print('\033[36m + \n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição'+ '\036[0m')
+print('\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
 
 paises=funcoes.normaliza(dados.DADOS)
 
-
 resposta=funcoes.sorteia_pais(paises)
-tentativa=input('Você tem', tentativas,'\nQual o seu palpite?')
+print('O pais foi escolhido!')
+
+term=''
+
+while vidas>0:
+    print('Você tem', vidas,'tentativas \nQual o seu palpite?' )
+    tentativa=input()
+    while tentativa not in paises.keys():
+        print('pais desconhecido')
+        print('Você tem', vidas,'\nQual o seu palpite?')
+        tentativa=input()
+
+    if tentativa != resposta:
+        vidas-=1
+        term+=tentativa
+        term+='\n'
+        term+= funcoes.haversine(6371,paises[tentativa]['geo']['latitude'],paises[resposta]['geo']['longitude'],paises[resposta]['geo']['latitude'],paises[tentativa]['geo']['longitude'])
+        print(term)
