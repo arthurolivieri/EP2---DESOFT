10626970572c6bbbcc845f62ea9f1cbf47325b3a
arthurolivieri <arthuro@al/insper.edu.br>
2022-05-11 22:25:20 -0300

    Desenvolvimento das dicas (criei uma lista onde se dá append das dicas pedidas)

--- jogo.py
+++ jogo.py
@@ -16,6 +16,7 @@ while jogarnovamente=='s':
     print('Um pais foi escolhido, tente adivinhar!\n')
     vidas=20
     paisesordenados=[]
+    dicasdadas = []
 
     while vidas>0:
         print('\nVocê tem', vidas,'tentativas' )
@@ -33,6 +34,9 @@ while jogarnovamente=='s':
             print('\nDistâncias:')
             for i in range(len(paisesordenados)):
                 print('    ', paisesordenados[i][1],'km ->',paisesordenados[i][0])
+            print('\nDicas:')
+            for d in dicasdadas:
+                print ('    ', d)
         
         if tentativa == resposta:
             print('\n*** Parabéns! Você acertou após', 21-vidas ,'tentativas!')
@@ -50,14 +54,19 @@ while jogarnovamente=='s':
             print('\nMercado de Dicas\n----------------------------------------\n1. Cor da bandeira  - custa 4 tentativas\n2. Letra da capital - custa 3 tentativas\n3. Área             - custa 6 tentativas\n4. População        - custa 5 tentativas\n5. Continente       - custa 7 tentativas\n0. Sem dica\n----------------------------------------')
             dica = int(input('Escolha sua opção [0 | 1 | 2 | 3 | 4 | 5]:'))
             if dica == 1:
+                dicasdadas.append('  - Cores da bandeira') #adicionar cor
                 vidas -= 4
             if dica == 2:
+                dicasdadas.append('  - Letras da capital: ', funcoes.sorteia_letra(paises[resposta]['capital'], ['-']))
                 vidas -= 3
             if dica == 3:
+                dicasdadas.append('  - Área: ', paises[resposta]['area'])
                 vidas -= 6
             if dica == 4:
+                dicasdadas.append('  - População: ', paises[resposta]['populacao'], 'pessoas')
                 vidas -= 5
             if dica == 5:
+                dicasdadas.append('  - Continente: ', paises[resposta]['continente'])
                 vidas -= 7
 
 
