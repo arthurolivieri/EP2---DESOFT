53a6ec73ec20de87bf19e46d9434c1abed05f8b0
arthurolivieri <arthuro@al/insper.edu.br>
2022-05-11 21:55:07 -0300

    Início das dicas(pergunta, print com as dicas e pagamento de tentativas)

--- jogo.py
+++ jogo.py
@@ -5,7 +5,6 @@ import funcoes
 import dados
 
 
-
 print('\n ============================ \n|                            |\n| Bem-vindo ao Insper Países |\n|                            |\n ==== Design de Software ==== \nComandos:\ndica       - entra no mercado de dicas\ndesisto    - desiste da rodada\ninventario - exibe sua posição')
 
 paises=funcoes.normaliza(dados.DADOS)
@@ -22,12 +21,12 @@ while jogarnovamente=='s':
         print('\nVocê tem', vidas,'tentativas' )
         tentativa=input( '\nQual o seu palpite? ')
 
-        while tentativa not in paises.keys() and tentativa != 'desisto':
+        while tentativa not in paises.keys() and tentativa != 'desisto' and tentativa != 'dica':
             print('pais desconhecido')
             print('\nVocê tem', vidas,'')
             tentativa=input('\nQual o seu palpite? ')
 
-        if tentativa != resposta and funcoes.esta_na_lista(tentativa,paisesordenados)==False and tentativa != 'desisto':
+        if tentativa != resposta and funcoes.esta_na_lista(tentativa,paisesordenados)==False and tentativa != 'desisto' and tentativa != 'dica':
             vidas-=1            
             dist= (int(funcoes.haversine(6371,paises[tentativa]['geo']['latitude'],paises[tentativa]['geo']['longitude'],paises[resposta]['geo']['latitude'],paises[resposta]['geo']['longitude'])))
             paisesordenados = funcoes.adiciona_em_ordem(tentativa,dist,paisesordenados)
@@ -46,7 +45,20 @@ while jogarnovamente=='s':
                 print('>>> Que deselegante desistir, o país era:', resposta)
                 jogarnovamente=input('\nJogar novamente? [s|n] ')
                 break
-            
+
+        if tentativa == 'dica':
+            print('\nMercado de Dicas\n----------------------------------------\n1. Cor da bandeira  - custa 4 tentativas\n2. Letra da capital - custa 3 tentativas\n3. Área             - custa 6 tentativas\n4. População        - custa 5 tentativas\n5. Continente       - custa 7 tentativas\n0. Sem dica\n----------------------------------------')
+            dica = int(input('Escolha sua opção [0 | 1 | 2 | 3 | 4 | 5]:'))
+            if dica == 1:
+                vidas -= 4
+            if dica == 2:
+                vidas -= 3
+            if dica == 3:
+                vidas -= 6
+            if dica == 4:
+                vidas -= 5
+            if dica == 5:
+                vidas -= 7
 
 
     if vidas<=0:
